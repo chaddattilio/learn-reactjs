fa47833f31b35c0baa4f0b852b86580b1db49968
Chad Dattilio <cdattilio@lyonscg.com>
2019-06-01 14:26:32 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import TodoItem from "./TodoItem";
+import todosData from "./todosData";
 
 function App() {
-  return (
-    <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-    </div>
-  );
+  const todosDataItems = todosData.map(item => (
+    <TodoItem key={item.id} todo={item} />
+  ));
+
+  return <div className="todo-list">{todosDataItems}</div>;
 }
 
 export default App;
